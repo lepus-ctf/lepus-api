3aa1491a3a62beaea65c7fa6346c26aed9bc534c
Nomuken <nomuken@spicadoll.com>
2015-06-15 09:19:04 +0900

    commit忘れてた><

--- lapus/serializers.py
+++ lapus/serializers.py
@@ -11,43 +11,25 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'ordering', 'created_at', 'updated_at')
 
 class QuestionSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=models.Category.objects.all())
-    
     class Meta:
         model = models.Question
-        fields = ('id', 'category', 'ordering', 'title', 'sentence', 'max_answers', 'max_failure', 'is_public', 'created_at', 'updated_at')
-        extra_kwargs = {'is_public' : {'write_only' : True}}
+        fields = ('id', 'category', 'ordering', 'title', 'sentence', 'max_answers', 'max_failure', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
 
-class FlagSerializer(serializers.ModelSerializer):
-    question = serializers.PrimaryKeyRelatedField(queryset=models.Question.objects.all())
-    
+class AdminFlagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Flag
         fields = ('id', 'flag', 'question', 'point', 'created_at', 'updated_at')
-        read_only_fields = ('created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
 
 class FileSerializer(serializers.ModelSerializer):
-    question = serializers.PrimaryKeyRelatedField(queryset=models.Question.objects.all())
-    
     class Meta:
         model = models.File
         fields = ('id', 'question', 'name', 'file', 'is_public', 'url', 'created_at', 'updated_at')
-        read_only_fields = ('url','created_at', 'updated_at')
-        extra_kwargs = {'is_public' : {'write_only' : True}}
+        read_only_fields = ('id', 'url','created_at', 'updated_at')
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Team
         fields = ('id', 'name', 'display_name',  'token', 'last_score_time', 'created_at', 'updated_at')
         read_only_fields = ('created_at', 'updated_at', 'token', 'id')
-        extra_kwargs = {'password' : {'write_only' : True}}
-    
-    def create(self, validated_data):
-        team = models.Team(
-                name = validated_data['name'],
-                display_name = validated_data['display_name']
-                )
-        team.set_password(validated_data['password'])
-        return team
-
-
