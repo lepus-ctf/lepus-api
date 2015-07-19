683156722ca1ea52837e9f08b71221e6930293ac
Nomuken <nomuken@spicadoll.com>
2015-07-19 20:49:05 +0900

    とりあえず埋めるだけ埋めた

--- lapus/serializers.py
+++ lapus/serializers.py
@@ -26,7 +26,7 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.File
         fields = ('id', 'question', 'name', 'file', 'is_public', 'url', 'created_at', 'updated_at')
-        read_only_fields = ('id', 'url','created_at', 'updated_at')
+        read_only_fields = ('id', 'url', 'created_at', 'updated_at')
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,12 +38,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ('id', 'username', 'is_staff', 'is_active', 'last_login', 'last_score_time', 'team', 'seat', 'display_name', 'points')
-        read_only_fields = ('id', 'username', 'last_login', 'last_score_time', 'points')
-        extra_kwargs = {'password'}
+        read_only_fields = ('id', 'username', 'last_login', 'last_score_time', 'points', 'created_at', 'updated_at')
+        extra_kwargs = {'password':{'write_only':True}}
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Answer
         fields = ('id', 'user', 'team', 'question', 'answer', 'is_correct')
-        read_only_fields = ('id', 'user', 'team', 'is_correct')
+        read_only_fields = ('id', 'user', 'team', 'is_correct', 'created_at', 'updated_at')
+        extra_kwargs = {'answer':{'write_only':True}}
+
+class AttackPointeSerialier(serializers.ModelSerializer):
+    class Meta:
+        model = models.AttackPoint
+        fields = ('id', 'user', 'team', 'question', 'taken', 'point')
+        read_only_fields = ('id', 'user', 'team', 'created_at', 'updated_at')
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Notice
+        fields = ('id', 'title', 'body')
 
