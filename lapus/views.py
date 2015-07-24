179eb2e7f8f39973794e2c36a01cf4e06eb9cad3
Nomuken <nomuken@spicadoll.com>
2015-07-24 20:31:33 +0900

    AuthなView

--- lapus/views.py
+++ lapus/views.py
@@ -1,9 +1,19 @@
 # encoding=utf-8
 from rest_framework import generics, permissions
-from .serializers import TeamSerializer
+from .serializers import TeamSerializer, UserSerializer
+
 
 class TeamListView(generics.ListAPIView):
     serializer_class = TeamSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
     permission_classes = permissions.AllowAny
+
+
+class AuthView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = permissions.IsAuthenticated
+
+
