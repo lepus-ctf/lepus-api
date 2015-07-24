ac026a254710cd96b5f29acb2651b16588c212ab
Nomuken <nomuken@spicadoll.com>
2015-07-24 20:19:41 +0900

    Teamのやつを書いてみた

--- lapus/views.py
+++ lapus/views.py
@@ -1,7 +1,9 @@
 # encoding=utf-8
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import TeamSerializer
 
 class TeamListView(generics.ListAPIView):
     serializer_class = TeamSerializer
     model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = permissions.AllowAny
