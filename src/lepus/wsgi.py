1ca6d05089e6be7b6ed8832777276e4a66a41703
Nomuken <nomuken@spicadoll.com>
2015-07-29 22:01:35 +0900

    残党を許すな

--- src/lepus/wsgi.py
+++ src/lepus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lapus project.
+WSGI config for lepus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lapus.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lepus.settings")
 
 application = get_wsgi_application()
