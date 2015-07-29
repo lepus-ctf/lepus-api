7ec5b8f268f29cbba26e505398b72ebd5492ba9b
Nomuken <nomuken@spicadoll.com>
2015-07-29 22:31:41 +0900

    あーなるほどね，そりゃあtype errorで落ちるよね

--- src/lepus/views.py
+++ src/lepus/views.py
@@ -8,49 +8,49 @@ class TeamListView(generics.ListAPIView):
     serializer_class = TeamSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.AllowAny
+    permission_classes = (permissions.AllowAny,)
 
 
 class AuthView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class QuestionView(generics.ListAPIView):
     serializer_class = QuestionSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CategoryView(generics.ListAPIView):
     serializer_class = CategorySerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class FileView(generics.ListAPIView):
     serializer_class = FileSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class AnswerView(generics.CreateAPIView):
     serializer_class = AnswerSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class NoticeView(generics.ListAPIView):
     serializer_class = NoticeSerializer
     model = serializer_class.Meta.model
     queryset = model.objects.all()
-    permission_classes = permissions.AllowAny
+    permission_classes = (permissions.AllowAny,)
 
 
 # TODO:AttackPointのAPIを開発する
