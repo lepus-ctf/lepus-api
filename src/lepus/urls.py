20c133922b0a82e2290bfe20970614f2507de17b
Nomuken <nomuken@spicadoll.com>
2015-07-29 22:01:57 +0900

    残党を許すな ~その2~

--- src/lepus/urls.py
+++ src/lepus/urls.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 
-"""lapus URL Configuration
+"""lepus URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
